6e95004fc1a313e7f3730fc399c7bace4ce3a4d6
Ana Perez Santos <a.jperez@yahoo.com>
2020-06-07 21:57:08 -0600

    Add test for Extension

--- spec/features/students/students_index_spec.rb
+++ spec/features/students/students_index_spec.rb
@@ -2,30 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Students Index Page" do
   describe "when I visit /students" do
-    it "I see a list of students and the number of professors each student has" do
-      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-      hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
-      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+    before(:each) do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+      @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
 
-      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
-      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
 
-      ProfessorStudent.create(student: harry, professor: snape)
-      ProfessorStudent.create(student: harry, professor: hagarid)
-      ProfessorStudent.create(student: harry, professor: lupin)
-      ProfessorStudent.create(student: malfoy, professor: hagarid)
-      ProfessorStudent.create(student: malfoy, professor: lupin)
-      ProfessorStudent.create(student: longbottom, professor: snape)
+      ProfessorStudent.create(student: @harry, professor: @snape)
+      ProfessorStudent.create(student: @harry, professor: @hagarid)
+      ProfessorStudent.create(student: @harry, professor: @lupin)
+      ProfessorStudent.create(student: @malfoy, professor: @hagarid)
+      ProfessorStudent.create(student: @malfoy, professor: @lupin)
+      ProfessorStudent.create(student: @longbottom, professor: @snape)
 
       visit "/students"
+    end
 
+    it "I see a list of students and the number of professors each student has" do
       expect(page).to have_content("Harry Potter: 3")
       expect(page).to have_content("Draco Malfoy: 2")
       expect(page).to have_content("Neville Longbottom: 1")
     end
+
+    it "I see students listed alphabetically" do
+      expect(@harry.name).to appear_before(@malfoy.name)
+      expect(@malfoy.name).to appear_before(@longbottom.name)
+    end
   end
 end
+
+# User Story Extension
+# On all index pages, all information is listed alphabetically.
+# Collapse
 
 # User Story 3 of 4
 # As a visitor,
